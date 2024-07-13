9e6f24994dd14699de254d32aa647db91b6ce9e7
Ashutosh Dubey <ashutoshdubey2825@gmail.com>
2024-07-13 14:09:53 +0530

    initial commit

--- models/blog.js
+++ models/blog.js
@@ -1,19 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const mongoose=require('mongoose');
-const schema=mongoose.Schema;
-const blogschema=new schema({
-    title:{
-        type:String,
-        required:true
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
     },
-    snippet:{
-        type:String,
-        required:true
+    snippet: {
+        type: String,
+        required: true
     },
-    body:{
-        type:String,
-        required:true
+    body: {
+        type: String,
+        required: true
     }
-},{timestamps:true});
-//model creation
-const Blog=mongoose.model('Blog',blogschema);
+}, { timestamps: true });
+
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+module.exports = Blog;
